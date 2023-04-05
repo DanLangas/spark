325577d112d6f1f8bdd465adad3250bd4858d960
DanLangas <DanLangas>
2023-04-05 10:19:03 +0000

    resolved the import koalas

--- PandasToKoalasTutorial.py
+++ PandasToKoalasTutorial.py
@@ -25,6 +25,10 @@
 
 import numpy as np
 import pandas as pd
+
+
+# COMMAND ----------
+
 import databricks.koalas as ks
 
 # COMMAND ----------
@@ -260,8 +264,8 @@ kdf = ks.DataFrame([[5.1, 3.5, 0], [4.9, 3.0, 0], [7.0, 3.2, 1],
                    columns=['length', 'width', 'species'])
 kdf.plot.scatter(x='length',
                  y='width',
-                 c='species',
-                 colormap='viridis')
+                 c='species')
+   #              colormap='viridis') # got an error here about colormap
 
 # COMMAND ----------
 
@@ -441,3 +445,7 @@ new_df.unpersist()
 
 with (df + df).cache() as df:
     df.explain()
+
+# COMMAND ----------
+
+
